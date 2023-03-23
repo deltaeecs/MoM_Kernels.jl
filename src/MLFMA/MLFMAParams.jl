1b27d915cc7c84f5ef4c2c8d2391491ee01f2c2d
deltaeecs <1225385871@qq.com>
2023-03-23 15:29:40 +0800

    更新八叉树插值算法设置相关函数

--- src/MLFMA/MLFMAParams.jl
+++ src/MLFMA/MLFMAParams.jl
@@ -12,21 +12,34 @@ Base.@kwdef mutable struct MLFMAParamsType{FT<:AbstractFloat}
     InterpolationMethod::Symbol = :Lagrange2Step
 end
 
+"""
+多层快速多极子的可调参数
+"""
 const MLFMAParams   =   MLFMAParamsType{typeof(Params.frequency)}(LEAFCUBESIZE = 0.23*Params.λ_0)
 
-function modiMLFMAParams!(λ_0::FT) where {FT<:AbstractFloat}
+"""
+设置叶层盒子边长
+"""
+function set_leafCubeSize!(λ_0::FT) where {FT<:AbstractFloat}
     MLFMAParams.LEAFCUBESIZE = 0.23*λ_0
 end
 
+"""
+设置插值算法
+"""
 function set_Interpolation_Method!(method)
     MLFMAParams.InterpolationMethod = method
     nothing
 end
 
-function get_Interpolation_Method(method)
-    if method == :Lagrange2Step
+"""
+获取插值算法
+"""
+get_Interpolation_Method(method::Symbol) = get_Interpolation_Method(Val(method))
+function get_Interpolation_Method(method::Union{Val{:Lagrange2Step}, Val{:Lagrange1Step}})
+    if method == Val(:Lagrange2Step)
         return LagrangeInterpInfo
-    elseif method == :Lagrange1Step
+    elseif method == Val(:Lagrange1Step)
         return LagrangeInterp1StepInfo
     else
         throw("插值方法选择出错")
