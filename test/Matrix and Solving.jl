152a6ef51074d803b507de29e29850e8b06bb9e3
xyhe <1225385871@qq.com>
2023-04-30 11:18:57 +0800

    修改测试用例以最快测试代码。

--- test/Matrix and Solving.jl
+++ test/Matrix and Solving.jl
@@ -1,8 +1,27 @@
-function test_matrix_solving(geosInfo, bfsInfo)
+function test_mat_solving(geosInfo, nbf)
+    
+    # 阻抗矩阵
+    Zmat = getImpedanceMatrix(geosInfo, nbf)
+
+    # 源
+    source  =   PlaneWave(π, 0, 0f0, 1f0)
+    # 激励向量
+    V    =   getExcitationVector(geosInfo, nbf, source);
+    @test true
+
+    # 迭代求解
+    ICoeff, _   =   solve(Zmat, V; solverT = :direct);
+    @test true
+
+    ICoeff
+
+end
+
+function test_opt_solving(geosInfo, bfsInfo; source = nothing)
     ## 快速算法
     # 计算阻抗矩阵（MLFMA计算），注意此处根据基函数在八叉树的位置信息改变了基函数顺序
     # Zopt, octree, ZnearCSC  =   getImpedanceOpt(geosInfo, bfsInfo);
-    nLevels, octree     =   getOctreeAndReOrderBFs!(geosInfo, bfsInfo)
+    nLevels, octree     =   getOctreeAndReOrderBFs!(geosInfo, bfsInfo; nInterp = 4)
     @test true
 
     # 叶层
@@ -19,14 +38,19 @@ function test_matrix_solving(geosInfo, bfsInfo)
     Zprel    =   sparseApproximateInversePl(ZnearCSC, leafLevel)
     @test true
 
-    source  =   PlaneWave(π, 0, 0f0, 1f0)
-
+    # 源
+    isnothing(source) && begin
+        source  =   MagneticDipole(;Iml = 1., phase = 0., orient = (π/2, π/2, 0))#PlaneWave(π, 0, 0f0, 1f0)
+    end
+    # 激励向量
     V    =   getExcitationVector(geosInfo, size(ZnearCSC, 1), source);
     @test true
 
+    # 迭代求解
     ICoeff, ch   =   solve(Zopt, V; solverT = :gmres, Pl = Zprel);
     @test true
 
+    # 原地迭代求解
     fill!(ICoeff, 0)
     ch   =   solve!(Zopt, ICoeff, V; solverT = :gmres!, Pl = Zprel);
     @test true
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,48 +1,61 @@
-using MoM_Basics, MoM_Kernels
+using MoM_Basics, MoM_Kernels, LinearAlgebra
 using Test
 include("Matrix and Solving.jl")
 include("PostProcessing.jl")
 
 SimulationParams.SHOWIMAGE = true
+setPrecision!(Float64)
 
 @testset "MoM_Kernels.jl" begin
+
+    # project path
+    proj_path = joinpath(@__DIR__, "..")
+
     @testset "Triangle, RWG" begin
-        filename = "../meshfiles/sphere_r1m_metal_1GHz.nas"
-        meshData, εᵣs   =  getMeshData(filename; meshUnit=:mm);
-        @test true
-        ngeo, nbf, geosInfo, bfsInfo =  getBFsFromMeshData(meshData)
-        @test true
         for IE in [:EFIE, :MFIE, :CFIE]
-            inputParameters(;frequency = 10e8, ieT = IE)
-            ICoeff = test_matrix_solving(geosInfo, bfsInfo)
+            inputParameters(;frequency = 20e8, ieT = IE)
+            filename = joinpath(proj_path, "meshfiles/Tri.nas")
+            meshData, εᵣs   =  getMeshData(filename; meshUnit=:mm);
+            @test true
+            ngeo, nbf, geosInfo, bfsInfo =  getBFsFromMeshData(meshData; sbfT = :RWG)
+            @test true
+
+            ICoeff = test_opt_solving(geosInfo, bfsInfo; source = PlaneWave(π, 0, 0f0, 1f0))
             test_postprocessing(ICoeff, geosInfo)
+
+            ICoeff2 = test_mat_solving(geosInfo, nbf)
+            test_postprocessing(ICoeff2, geosInfo)
         end
     end
 
     @testset "Terahedron, PWC and SWG" begin
 
-        filename = "../meshfiles/sphereShellTetra50mm.nas"
+        filename = joinpath(proj_path, "meshfiles/Tetra.nas")
         meshData, εᵣs   =  getMeshData(filename; meshUnit=:mm);
         @test true
 
-        @testset "PWC, SWG" for vbfT in [:PWC, :SWG]
+        @testset "PWC, SWG" for vbfT in [:SWG, :PWC]#, 
 
             ngeo, nbf, geosInfo, bfsInfo =  getBFsFromMeshData(meshData, vbfT = vbfT)
             @test true
 
-            setGeosPermittivity!(geosInfo, 2 + 0.001im)
+            setGeosPermittivity!(geosInfo, 2(1-0.001im))
             @test true
 
-            inputParameters(;frequency = 5e8, ieT = :EFIE)
-            ICoeff = test_matrix_solving(geosInfo, bfsInfo)
+            inputParameters(;frequency = 20e8, ieT = :EFIE)
+            ICoeff = test_opt_solving(geosInfo, bfsInfo)
             test_postprocessing(ICoeff, geosInfo)
+
+            ICoeff2 = test_mat_solving(geosInfo, nbf)
+            test_postprocessing(ICoeff2, geosInfo)
+
         end
     end
 
     
     @testset "Hexadron, PWC and RBF" begin
 
-        filename = "../meshfiles/sphereShellHexa50mm.nas"
+        filename = joinpath(proj_path, "meshfiles/Hexa.nas")
         meshData, εᵣs   =  getMeshData(filename; meshUnit=:mm);
         @test true
 
@@ -51,32 +64,89 @@ SimulationParams.SHOWIMAGE = true
             ngeo, nbf, geosInfo, bfsInfo =  getBFsFromMeshData(meshData, vbfT = vbfT)
             @test true
 
-            setGeosPermittivity!(geosInfo, 2 + 0.001im)
+            setGeosPermittivity!(geosInfo, 2(1-0.001im))
             @test true
 
-            inputParameters(;frequency = 5e8, ieT = :EFIE)
-            ICoeff = test_matrix_solving(geosInfo, bfsInfo)
+            inputParameters(;frequency = 20e8, ieT = :EFIE)
+            ICoeff = test_opt_solving(geosInfo, bfsInfo)
             test_postprocessing(ICoeff, geosInfo)
+
+            ICoeff2 = test_mat_solving(geosInfo, nbf)
+            test_postprocessing(ICoeff2, geosInfo)
 
         end
     end
 
-
     @testset "Tetra + Hexadron, PWC" begin
 
-        filename = "../meshfiles/sphereShellHT50mm.nas"
+        filename = joinpath(proj_path, "meshfiles/TetraHexa.nas")
+
         meshData, εᵣs   =  getMeshData(filename; meshUnit=:mm);
         @test true
 
         ngeo, nbf, geosInfo, bfsInfo =  getBFsFromMeshData(meshData, vbfT = :PWC)
         @test true
 
-        setGeosPermittivity!(geosInfo, 2 + 0.001im)
+        setGeosPermittivity!(geosInfo, 2(1-0.001im))
         @test true
 
-        inputParameters(;frequency = 5e8, ieT = :EFIE)
-        ICoeff = test_matrix_solving(geosInfo, bfsInfo)
+        inputParameters(;frequency = 20e8, ieT = :EFIE)
+        ICoeff = test_opt_solving(geosInfo, bfsInfo)
         test_postprocessing(ICoeff, geosInfo)
+        ICoeff2 = test_mat_solving(geosInfo, nbf)
+        test_postprocessing(ICoeff2, geosInfo)
+
+    end
+
+    @testset "Tri + Tetra, RWG + SWG, RWG + PWC" begin
+
+        filename = joinpath(proj_path, "meshfiles/TriTetra.nas")
+
+        meshData, εᵣs   =  getMeshData(filename; meshUnit=:mm);
+        @test true
+
+        @testset "RWG + SWG, RWG + PWC" for vbfT in [:SWG, :PWC]
+
+            ngeo, nbf, geosInfo, bfsInfo =  getBFsFromMeshData(meshData, sbfT = :RWG, vbfT = vbfT)
+            @test true
+
+            setGeosPermittivity!(geosInfo, 2(1-0.001im))
+            @test true
+
+            inputParameters(;frequency = 20e8, ieT = :EFIE)
+            ICoeff = test_opt_solving(geosInfo, bfsInfo)
+            test_postprocessing(ICoeff, geosInfo)
+
+            ICoeff2 = test_mat_solving(geosInfo, nbf)
+            test_postprocessing(ICoeff2, geosInfo)
+
+        end
+
+    end
+
+    @testset "Tri + Hexa, RWG + PWC, RWG + RBF" begin
+
+        filename = joinpath(proj_path, "meshfiles/TriHexa.nas")
+
+        meshData, εᵣs   =  getMeshData(filename; meshUnit=:mm);
+        @test true
+
+        @testset "RWG + SWG, RWG + PWC" for vbfT in [:PWC, :RBF]
+
+            ngeo, nbf, geosInfo, bfsInfo =  getBFsFromMeshData(meshData, sbfT = :RWG, vbfT = vbfT)
+            @test true
+
+            setGeosPermittivity!(geosInfo, 2(1-0.001im))
+            @test true
+
+            inputParameters(;frequency = 20e8, ieT = :EFIE)
+            ICoeff = test_opt_solving(geosInfo, bfsInfo)
+            test_postprocessing(ICoeff, geosInfo)
+
+            ICoeff2 = test_mat_solving(geosInfo, nbf)
+            test_postprocessing(ICoeff2, geosInfo)
+
+        end
 
     end
 
