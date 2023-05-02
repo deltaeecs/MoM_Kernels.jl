9d68a62e853366b6967fee310b20bda29c66e41c
xyhe <1225385871@qq.com>
2023-05-02 12:06:49 +0800

    update docs of some functions

--- src/Inputs.jl
+++ src/Inputs.jl
@@ -1,6 +1,8 @@
 """
-用于输入频率参数，修改其它仿真参数的函数
-输入积分方程类型参数
+    inputParameters(;args...)
+
+用于输入仿真参数，并修改奇异性处理中频率相关常量。
+详见 [`inputBasicParameters`](@ref) 和 [`modiSingularityRelatedConsts!`](@ref)。
 """
 function inputParameters(;args...)
     inputBasicParameters(;args...)
